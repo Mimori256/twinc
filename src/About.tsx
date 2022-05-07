dfb16201d238e4b7c0ef3b64b03b98087477ca34
Mimori256 <mimorimori256@gmail.com>
2022-05-07 19:47:50 +0900

    修正

--- src/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div id="content">これはAbout</div>;
-};
-
-export default About;
--- src/Help.tsx
+++ src/Help.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { SideMenu } from "./SideMenu";
 
 const Help: React.FC = () => {
   return (

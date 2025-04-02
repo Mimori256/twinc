5dae97ddf0b39b7098749b1bda9d14a47260ce72
Mimori <mimorimori256@gmail.com>
2025-04-02 13:59:59 +0900

    organize deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 import linaria from "@linaria/vite";
 
 export default defineConfig({
-  // depending on your application, base can also be "/"
   base: "",
   plugins: [react(), viteTsconfigPaths(), linaria()],
   server: {
-    // this ensures that the browser opens upon server start
     open: true,
-    // this sets a default port to 3000
     port: 3000,
   },
 });
